7aa78ff6bad4cca83d1c9335d2c1bcdcd4cf1c18
batuaygun <batu.aygn@gmail.com>
2022-12-12 16:53:12 +0300

    final

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const pageRoute = require("./routes/pageRoutes");
+const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 
 //Connect DB
 mongoose.connect("mongodb://localhost/smartedu-db").then(() => {
@@ -21,6 +23,8 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 //Routes
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
+app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = 3000;
 app.listen(port, () => {
